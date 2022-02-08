1b48faa3c908d3447d58eff74b3d43acd2f84977
quocanhbaoly123 <99275199+quocanhbaoly123@users.noreply.github.com>
2022-02-08 23:22:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Demo",
+    introTitle: 'Trần Thị Bảo Ly',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
